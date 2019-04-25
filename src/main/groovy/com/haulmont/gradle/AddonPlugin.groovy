25fd217c0eee899694da74f9a1f5e3623bcea4ff
zaharchenko <zaharchenko@haulmont.com>
2019-04-25 13:27:09 +0400

    CCI-7 Add javadoc configuration to gradle addon plugin

--- src/main/groovy/com/haulmont/gradle/AddonPlugin.groovy
+++ src/main/groovy/com/haulmont/gradle/AddonPlugin.groovy
@@ -30,38 +30,7 @@ class AddonPlugin implements Plugin<Project> {
         project.afterEvaluate { Project p ->
             if (project != project.rootProject) {
                 if (project.name.endsWith("-core")) {
-                    def createDb = project.tasks.findByName("createDb")
-                    def dbName = createDb.dbName
-                    def testDbName = createDb.dbName + "-test"
-                    def hsqlPort = DEFAULT_DB_PORT
-                    def host = createDb.host
-                    if (host.contains(":")) {
-                        hsqlPort = host.substring(host.lastIndexOf(":") + 1) as Integer
-                    }
-                    if (!project.getTasks().findByName("startDb")) {
-                        def startDb = project.task([type: Class.forName('com.haulmont.gradle.task.db.CubaHsqlStart')], "startDb")
-                        startDb.dbName = dbName
-                        startDb.dbPort = hsqlPort
-                    }
-                    if (!project.getTasks().findByName("stopDb")) {
-                        def stopDb = project.task([type: Class.forName('com.haulmont.gradle.task.db.CubaHsqlStop')], "stopDb")
-                        stopDb.dbName = dbName
-                        stopDb.dbPort = hsqlPort
-                    }
-                    if (!project.getTasks().findByName("startTestDb")) {
-                        def startTestDb = project.task([type: Class.forName('com.haulmont.gradle.task.db.CubaHsqlStart')], "startTestDb")
-                        startTestDb.dbName = testDbName
-                        startTestDb.dbPort = hsqlPort
-                    }
-                    if (!project.getTasks().findByName("stopTestDb")) {
-                        def stopTestDb = project.task([type: Class.forName('com.haulmont.gradle.task.db.CubaHsqlStop')], "stopTestDb")
-                        stopTestDb.dbName = testDbName
-                        stopTestDb.dbPort = hsqlPort
-                    }
-                    if (!project.getTasks().findByName("killDb")) {
-                        def killProcess = project.task([type: KillProcessTask], "killDb")
-                        killProcess.port = hsqlPort
-                    }
+                    configureDbTasks(project)
                 }
                 if (!project.getTasks().findByName("javadoc")) {
                     def javadoc = project.task("javadoc")
@@ -75,6 +44,41 @@ class AddonPlugin implements Plugin<Project> {
                     configureJavaDoc(project, javadoc)
                 }
             }
+        }
+    }
+
+    private void configureDbTasks(Project project) {
+        def createDb = project.tasks.findByName("createDb")
+        def dbName = createDb.dbName
+        def testDbName = createDb.dbName + "-test"
+        def hsqlPort = DEFAULT_DB_PORT
+        def host = createDb.host
+        if (host.contains(":")) {
+            hsqlPort = host.substring(host.lastIndexOf(":") + 1) as Integer
+        }
+        if (!project.getTasks().findByName("startDb")) {
+            def startDb = project.task([type: Class.forName('com.haulmont.gradle.task.db.CubaHsqlStart')], "startDb")
+            startDb.dbName = dbName
+            startDb.dbPort = hsqlPort
+        }
+        if (!project.getTasks().findByName("stopDb")) {
+            def stopDb = project.task([type: Class.forName('com.haulmont.gradle.task.db.CubaHsqlStop')], "stopDb")
+            stopDb.dbName = dbName
+            stopDb.dbPort = hsqlPort
+        }
+        if (!project.getTasks().findByName("startTestDb")) {
+            def startTestDb = project.task([type: Class.forName('com.haulmont.gradle.task.db.CubaHsqlStart')], "startTestDb")
+            startTestDb.dbName = testDbName
+            startTestDb.dbPort = hsqlPort
+        }
+        if (!project.getTasks().findByName("stopTestDb")) {
+            def stopTestDb = project.task([type: Class.forName('com.haulmont.gradle.task.db.CubaHsqlStop')], "stopTestDb")
+            stopTestDb.dbName = testDbName
+            stopTestDb.dbPort = hsqlPort
+        }
+        if (!project.getTasks().findByName("killDb")) {
+            def killProcess = project.task([type: KillProcessTask], "killDb")
+            killProcess.port = hsqlPort
         }
     }
 
